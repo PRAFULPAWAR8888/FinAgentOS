c7bd89fe2fc30e517babd68dbf123ce4a19a9673
Praful Pawar <pawarpraful950@gmail.com>
2026-09-15 18:19:01 +0530

    updates

---
+++ apps/web/streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+st.set_page_config(
+    page_title="FinAgentOs",
+    page_icon= "💰",
+)
+
+st.title("FinAgentOs")
+st.write("Governed Multi-Agent AI Platform for Intelligent Financial Services")
